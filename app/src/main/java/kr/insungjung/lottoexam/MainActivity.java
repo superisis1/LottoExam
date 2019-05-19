86101df0dd372d201637bac5cbe53c72d09082df
Insung Jung <superisis@gmail.com>
2019-05-19 20:55:46 +0900

    Initial commit

---
+++ app/src/main/java/kr/insungjung/lottoexam/MainActivity.java
@@ -0,0 +1,37 @@
+package kr.insungjung.lottoexam;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+
+import kr.insungjung.lottoexam.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+    }
+}
